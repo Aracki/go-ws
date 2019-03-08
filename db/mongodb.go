ef1d9be6c8a475a5fd2af348c770a8fce8be104b
Aracki <aracki.ivan@gmail.com>
2019-03-08 14:27:09 +0100

    Add mongo init func; Read url/port from environment

--- db/mongodb.go
+++ db/mongodb.go
@@ -2,25 +2,40 @@ package db
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"log"
+	"os"
 	"time"
 
 	"github.com/mongodb/mongo-go-driver/bson"
 	"github.com/mongodb/mongo-go-driver/mongo"
 )
 
-var dbUrl = "mongodb://localhost:27017"
+var client *mongo.Client
+
+func init() {
+
+	dbUrl := "mongodb://" + os.Getenv("host") + ":" + os.Getenv("port")
+
+	ctx, _ := context.WithTimeout(context.Background(), 3*time.Second)
+	var err error
+	client, err = mongo.Connect(ctx, dbUrl)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		log.Printf("connected to %s\n", dbUrl)
+	}
+}
 
 func InsertNumber(num float32) error {
 
-	ctx, _ := context.WithTimeout(context.Background(), 3*time.Second)
-	client, err := mongo.Connect(ctx, dbUrl)
-	if err != nil {
-		return err
+	if client == nil {
+		return errors.New("cannot create mongo client")
 	}
 
+	ctx, _ := context.WithTimeout(context.Background(), 3*time.Second)
 	collection := client.Database("testing").Collection("numbers")
-
 	res, err := collection.InsertOne(ctx, bson.M{"name": "pi", "value": num})
 	if err != nil {
 		return err
@@ -33,12 +48,11 @@ func InsertNumber(num float32) error {
 
 func GetAllValues() (values []interface{}, err error) {
 
-	ctx, _ := context.WithTimeout(context.Background(), 3*time.Second)
-	client, err := mongo.Connect(ctx, dbUrl)
-	if err != nil {
-		return nil, err
+	if client == nil {
+		return nil, errors.New("cannot create mongo client")
 	}
 
+	ctx, _ := context.WithTimeout(context.Background(), 3*time.Second)
 	collection := client.Database("testing").Collection("numbers")
 	cur, err := collection.Find(ctx, bson.D{})
 	if err != nil {
