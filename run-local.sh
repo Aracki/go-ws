279d35e533b6e4ad2b7218bd20bfbec5acdc35b7
Aracki <aracki.ivan@gmail.com>
2019-04-02 23:51:12 +0200

    Refactor run-local.sh with echos

--- run-local.sh
+++ run-local.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+echo "building go binary..."
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -a -o go-ws cmd/go-ws/main.go
+
+echo "building docker image..."
 docker build --no-cache -t go-web-server .
-docker run --name go-ws -p 8080:8080 --rm go-web-server:latest
+
+echo "deleting go-ws binary..."
 rm go-ws
+
+echo "running docker container..."
+docker run --name go-ws -p 8080:8080 --rm go-web-server:latest
